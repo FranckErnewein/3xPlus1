c46a960618a8fa08e127880105783ac57058b085
FranckErnewein <franck.ernewein@gmail.com>
2023-06-13 22:14:50 +0200

    init project with core function

---
+++ app/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+
+threeXPlusOne :: Int -> Int
+threeXPlusOne x
+  | even x = x `div` 2
+  | otherwise = (3 * x) + 1
+
+seq3xp1 :: Int -> [Int]
+seq3xp1 1 = [] 
+seq3xp1 x = (threeXPlusOne x):(seq3xp1 (threeXPlusOne x))
+
+main :: IO ()
+main = do
+  x <- getLine
+  print $ seq3xp1 (read x)
+  
